95191f05530918ecad309d74c6823769bf856679
Marcela Distefano <marcelitadis@gmail.com>
2024-05-14 21:49:18 -0300

    sin type of failure

--- tpot_export_default_pipeline.py
+++ tpot_export_default_pipeline.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -9,8 +9,8 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=42)
 
-# Average CV score on the training set was: 0.9865557096625057
-exported_pipeline = DecisionTreeClassifier(criterion="entropy", max_depth=5, min_samples_leaf=6, min_samples_split=4)
+# Average CV score on the training set was: 0.9943337693533291
+exported_pipeline = GradientBoostingClassifier(learning_rate=0.01, max_depth=8, max_features=0.5, min_samples_leaf=12, min_samples_split=20, n_estimators=100, subsample=0.55)
 # Fix random state in exported estimator
 if hasattr(exported_pipeline, 'random_state'):
     setattr(exported_pipeline, 'random_state', 42)
